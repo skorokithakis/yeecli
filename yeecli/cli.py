5e1b6cbe856b04473454649715e5e39b9af3c9da
pklapperich <pklapperich@user.noreply.github.com>
2018-05-15 22:44:35 -0500

    Add stderr output to hex_to_color rather than just setting red without telling the user WTF happened

--- yeecli/cli.py
+++ yeecli/cli.py
@@ -30,6 +30,7 @@ def hex_color_to_rgb(color):
     try:
         red, green, blue = tuple(int(color[i:i + 2], 16) for i in (0, 2, 4))
     except (TypeError, ValueError):
+        print('Unrecognized color', file=sys.stderr)
         red, green, blue = (255, 0, 0)
     return red, green, blue
 
